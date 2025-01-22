07115009bff52ecb1c467bb11e7aeec7b0953e92
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 15:08:03 +0530

    committed

---
+++ Fifth.java
@@ -0,0 +1,9 @@
+public class Fifth {
+    public static void main(String[] args) {
+        int tp=14;
+        int s=3;
+        int pps=tp/s;
+        int rp=tp%s;
+        System.out.println("The Pen Per Student is "+pps+" and the remaining pen not distributed is "+rp);
+    }
+}
