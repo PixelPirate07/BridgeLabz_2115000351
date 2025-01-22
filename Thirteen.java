a89cbbb8f6d4aeba43f487b0f5f0d10d21641eaf
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 15:54:10 +0530

    committed

---
+++ Thirteen.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Thirteen{
+    public static void main(String[] args) {
+        double p;
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextDouble();
+        double s=p/4;
+        System.out.println("The length of the side is "+s+" whose perimeter is "+p);
+    }
+}
