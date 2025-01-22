3e1882275704a971ea20bb32712208fd0f09cae1
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 14:54:35 +0530

    committed

---
+++ Third.java
@@ -0,0 +1,8 @@
+public class Third {
+    public static void main(String[] args) {
+        double k = 10.8;
+        double c = 1.6;
+        double m = k*c;
+        System.out.println("The distance " + k + " km in miles is " + m);
+    }
+}
