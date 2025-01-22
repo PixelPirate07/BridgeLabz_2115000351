cd2e432a9647b8caf76083d9d491a1ed1e3863a8
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 19:31:08 +0530

    committed

---
+++ Twentytwo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Twentytwo{
+    public static void main(String[] args) {
+        double sal,b,ti;
+        Scanner sc=new Scanner(System.in);
+        sal=sc.nextDouble();
+        b=sc.nextDouble();
+        ti=sal+b;
+        System.out.println("The salary is INR "+sal+" and bonus is INR "+b+". Hence Total Income is INR "+ti);
+    }
+}
