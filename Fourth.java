35e731271138c67ab160d1133eb8f2a09ebafcd0
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 15:03:25 +0530

    committed

---
+++ Fourth.java
@@ -0,0 +1,9 @@
+public class Fourth {
+    public static void main(String[] args) {
+        int cop =129,sep=191;
+        int p=sep-cop;
+        double pp=(p/(double)cop)*100;
+        System.out.println("Cost Price is "+cop+ "and Selling Price is " + sep);
+        System.out.println("Profitis "+p+ "and Profit Percentage is " +pp+ "%");
+    }
+}
