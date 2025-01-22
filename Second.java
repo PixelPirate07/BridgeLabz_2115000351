96198b2e5846b19dff01a56595f5c48fe2b28a91
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 14:49:16 +0530

    committed

---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    public static void main(String[] args) {
+        int m = 94, p = 95, c = 96;
+        int tm = m+p+c;
+        double av = tm / 3.0;
+        System.out.println("Sam's average mark in PCM is " + av);
+    }
+}
