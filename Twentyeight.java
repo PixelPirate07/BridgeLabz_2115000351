f7b0139dcd22e16171881f8b33bfafcac8f7a984
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-22 20:15:13 +0530

    committed

---
+++ Twentyeight.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Twentyeight{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the weight in pounds: ");
+        double w=sc.nextDouble();
+        double wk=w/2.2;
+        System.out.println("The weight of the person in pounds is "+w+" and in kilograms is "+wk+".");
+                           
+    }
+}
