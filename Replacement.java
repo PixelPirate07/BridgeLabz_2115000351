5cf35e00fe6805891c1463682c9caf33d70d89c6
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-30 14:50:43 +0530

    committed

--- Replacement.java
+++ Replacement.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Replacement{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a string ");
         String str=sc.nextLine();
@@ -8,7 +8,28 @@ public class Replacement{
         String wordtoreplace=sc.nextLine();
         System.out.println("Enter the replacement word ");
         String replaceword=sc.nextLine();
-        String result=str.replace(wordtoreplace,replaceword);
-        System.out.println("Final string is "+result);
+        String result="";
+        int i=0;
+        while(i<str.length()){
+            boolean match=true;
+            if(i+wordtoreplace.length()<=str.length()){
+                for(int j=0;j<wordtoreplace.length();j++){
+                    if(str.charAt(i+j)!=wordtoreplace.charAt(j)){
+                        match=false;
+                        break;
+                    }
+                }
+            }else{
+                match=false;
+            }
+            if(match){
+                result+=replaceword;
+                i+=wordtoreplace.length();
+            }else{
+                result+=str.charAt(i);
+                i++;
+            }
+        }
+        System.out.println("Final string is- "+result);
     }
 }
