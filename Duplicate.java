3fe950f58e2066ad48960d97f552d005a78eb1b1
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-30 14:48:46 +0530

    committed

--- Duplicate.java
+++ Duplicate.java
@@ -1,27 +1,31 @@
 import java.util.*;
 public class Duplicate{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter string ");
-		String str=sc.nextLine();
-		StringBuilder s=new StringBuilder(str);
-		StringBuilder str2=new StringBuilder();
-		char c1,c2;
-		s=s.reverse();
-		for(int i=0;i<s.length();i++){
-		    c1=s.charAt(i);
-		    int count=0;
-		    for(int j=i+1;j<s.length();j++){
-		       c2=s.charAt(j);
-		       if(c1==c2){
-		           count++;
-		       }
-		    }
-		    if(count==0){
-		        str2=str2.append(c1);
-		    }
-		}
-		str2=str2.reverse();
-		System.out.println("After removing duplicates string is "+str2);
-	}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String str=sc.nextLine();
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev+=str.charAt(i);
+        }
+        String result="";
+        for(int i=0;i<rev.length();i++){
+            char c=rev.charAt(i);
+            boolean duplicate=false;
+            for(int j=0;j<result.length();j++){
+                if(result.charAt(j)==c){
+                    duplicate=true;
+                    break;
+                }
+            }
+            if(!duplicate){
+                result+=c;
+            }
+        }
+        String str2="";
+        for(int i =result.length()-1;i>=0;i--){
+            str2+=result.charAt(i);
+        }
+        System.out.println("Final string is " +str2);
+    }
 }
