e45e9b7542cb4ec11b86a91324d09db600feba12
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-30 14:50:19 +0530

    committed

--- Anagram.java
+++ Anagram.java
@@ -1,25 +1,32 @@
 import java.util.*;
 public class Anagram{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the first string: ");
-        String str1=sc.nextLine();
-        System.out.println("Enter the second string: ");
-        String str2=sc.nextLine();
-        if (areAnagrams(str1,str2)){
-            System.out.println("Anagram");
-        } else {
-            System.out.println("Anagram");
-        }
-    }
-    public static boolean areAnagrams(String str1,String str2) {
-        if(str1.length()!=str2.length()){
+    public static boolean areAnagrams(String str,String str2){
+        if (str.length()!=str2.length()){
             return false;
         }
-        char[] arr1=str1.toCharArray();
-        char[] arr2=str2.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1,arr2);
+        int[] c1=new int[256];
+        int[] c2=new int[256];
+        for(int i=0;i<str.length();i++){
+            c1[str.charAt(i)]++;
+            c2[str2.charAt(i)]++;
+        }
+        for(int i=0;i<256;i++){
+            if(c1[i]!=c2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter string ");
+        String str=sc.nextLine();
+        System.out.println("Enter string ");
+        String str2=sc.nextLine();
+        if(areAnagrams(str,str2)){
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
     }
 }
