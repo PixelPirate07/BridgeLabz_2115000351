79f0d8629c316f1bcc0f2a274461cab0a945a3fb
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-30 14:50:59 +0530

    committed

--- Longest.java
+++ Longest.java
@@ -1,16 +1,25 @@
 import java.util.*;
 public class Longest{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter string ");
-		String str=sc.nextLine();
-		String[] words=str.split(" ");
-		String longest="";
-		for(String word:words){
-		   if(word.length()>longest.length()){
-		       longest=word;
-		   }
-		}
-		System.out.println("Longest word is "+longest);
-	}
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter string ");
+        String str=sc.nextLine();
+        String longest="";
+        String word="";
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch!=' '){
+                word+=ch;
+            }else if(!word.equals("")){
+                if(word.length()>longest.length()){
+                    longest=word;
+                }
+                word="";
+            }
+        }
+        if(!word.equals("") && word.length()>longest.length()){
+            longest=word;
+        }
+        System.out.println("Longest word is "+longest);
+    }
 }
